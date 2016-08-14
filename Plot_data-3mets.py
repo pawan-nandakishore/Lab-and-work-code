7fefb8b12100fc7b6539da1cc5326b6a7a723373
pawan-nandakishore <pawan.nandakishore@gmail.com>
2016-08-14 23:58:35 +0530

    Add files via upload

---
+++ Plot_data-3mets.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 11 12:02:07 2016
+
+@author: pawan
+"""
+
+import numpy as np
+import csv 
+from sklearn.neighbors import DistanceMetric
+import matplotlib.pyplot as plt 
+import string
+
+#with open('eggs.csv', 'rb') as csvfile:
+#...     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#...     for row in spamreader:
+#...         print ', '.join(row)
+
+fileLoc='/media/pawan/0B6F079E0B6F079E/PYTHON_SCRIPTS/Shashi_detection/Metronome_error_detection/Test_condition_1/Data files/August3rd_trial2 /position_data_multiple_mets.csv' 
+
+# OPEN FILE AND THEN READ EACH ROW. THE LIST ALL_DATA WILL BE A LIST OF LISTS WHERE ALL THE ROW DATA WILL BE APPENDED 
+all_data = []
+with open(fileLoc)  as csvfile: 
+    
+     readfile = csv.reader(csvfile, delimiter = ',')
+     for row in readfile: 
+         all_data.append(map(float,row[:14]))
+         
+
+all_data = np.array(all_data)         
+coordinates = all_data[:,2:6]
+circles=coordinates[:,0:2]
+boxes = coordinates[:,2:4]
+
+
+all_distances =[]
+dist = DistanceMetric.get_metric('euclidean')
+
+for x in range(0,len(all_data)): 
+    dist_mat = dist.pairwise([boxes[x],circles[x]])
+    all_distances.append([all_data[x,0],all_data[x,1], dist_mat[0][1]])
+
+all_distances =np.array(all_distances)
+
+number_mets = np.unique(all_data[:,1])
+
+len_met_pos_list = len(all_distances)/len(number_mets)
+
+
+
+
+met_data_boxes = boxes[0:-1:3,:]
+for x in range(1,len(number_mets)-1):
+ met_data_boxes= np.hstack((met_data_boxes,boxes[x:-1:3,:]))
+
+copy_boxes = np.vstack((boxes[2:-1:3,:], boxes[-1,:]))
+met_data_boxes= np.hstack((met_data_boxes,copy_boxes))
+
+
+met_data_circles = circles[0:-1:3,:]
+for x in range(1,len(number_mets)-1):
+ met_data_circles= np.hstack((met_data_circles,circles[x:-1:3,:]))
+
+copy_circles = np.vstack((circles[2:-1:3,:], circles[-1,:]))
+met_data_circles= np.hstack((met_data_circles,copy_circles))
+
+
+bob_direction = met_data_circles[:,0:-1:2]- met_data_boxes[:,0:-1:2]
+ 
+bob_direction[bob_direction<0] =-1
+bob_direction[bob_direction>0] =1
+
+
+
+
+
