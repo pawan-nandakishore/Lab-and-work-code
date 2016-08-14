3c54ca2f7dcb956bed0d7e04de3b071711e4d419
pawan-nandakishore <pawan.nandakishore@gmail.com>
2016-08-15 01:55:55 +0530

    Add files via upload

--- Plot_data-3mets.py
+++ Plot_data-3mets.py
@@ -48,7 +48,7 @@ len_met_pos_list = len(all_distances)/len(number_mets)
 
 
 
-
+## SORT THE FULL VALUES SUCH THAT YOU GET DATA FOR EACH OF THE METRONOMES
 met_data_boxes = boxes[0:-1:3,:]
 for x in range(1,len(number_mets)-1):
  met_data_boxes= np.hstack((met_data_boxes,boxes[x:-1:3,:]))
@@ -61,16 +61,33 @@ met_data_circles = circles[0:-1:3,:]
 for x in range(1,len(number_mets)-1):
  met_data_circles= np.hstack((met_data_circles,circles[x:-1:3,:]))
 
+ 
 copy_circles = np.vstack((circles[2:-1:3,:], circles[-1,:]))
 met_data_circles= np.hstack((met_data_circles,copy_circles))
+
+
+
+met_data_dists = all_distances[0:-1:3,:]
+for x in range(1,len(number_mets)-1):
+ met_data_dists= np.hstack((met_data_dists,all_distances[x:-1:3,:]))
+ 
+ 
+copy_distances = np.vstack((all_distances[2:-1:3,:], all_distances[-1,:]))
+met_data_dists= np.hstack((met_data_dists,copy_distances))
 
 
 bob_direction = met_data_circles[:,0:-1:2]- met_data_boxes[:,0:-1:2]
  
 bob_direction[bob_direction<0] =-1
-bob_direction[bob_direction>0] =1
+bob_direction[bob_direction>=0] =1
 
+just_distances =  np.hstack((met_data_dists[:,2:-1:3],met_data_dists[:,-1:]))
 
+dist_with_direction = np.multiply(just_distances,bob_direction)
 
+dist_with_direction_copy = dist_with_direction.copy()
 
+dist_with_direction[dist_with_direction<0] = np.subtract(dist_with_direction[dist_with_direction<0],-67) 
+
+dist_with_direction[dist_with_direction>0]= np.subtract(dist_with_direction[dist_with_direction>0],67) 
 
